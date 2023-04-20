301185c6787d656114b3a6de2f0484d78ea737ae
jarome <jaromejiang@yunify.com>
2023-04-21 00:15:36 +0800

    update init version

--- content_script.js
+++ content_script.js
@@ -1,4 +1,29 @@
-// Add additional features to ChatGPT chat page here
-// Example: auto-scroll to the bottom of the chat window
-const chatWindow = document.querySelector('.chat-window');
-chatWindow.scrollTop = chatWindow.scrollHeight;
+/**
+ * 向页面增加样式文件
+ * @param {*} url 
+ */
+function addStyle(href) {
+    var link = document.createElement('link');
+    link.href = chrome.runtime.getURL(href);
+    link.type = 'text/css';
+    link.rel = 'stylesheet';
+    link.media = 'all';
+    document.getElementsByTagName('HEAD')[0].appendChild(link);
+}
+
+/**
+ * 向页面增加js脚本文件
+ * @param {*} src 
+ */
+function addScript(src) {
+    var nodeElement = document.getElementsByTagName('body')[0];
+    var script = document.createElement('script');
+    script.setAttribute('type', 'text/javascript');
+    script.setAttribute('src', src);
+    nodeElement.appendChild(script);
+}
+
+
+// 以下是执行文件
+addStyle('index.css')
+addScript(chrome.runtime.getURL('index.js'))
